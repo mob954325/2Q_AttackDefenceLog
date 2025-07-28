e1462563a0a18f8f6e70a58fdc783cb6f967f2bf
Popimuty <bin9825aaa@gmail.com>
2025-07-28 18:11:45 +0900

    asdasd

---
+++ BinGameTestContents/GameLogicTestScene/GameLogicTestScene.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include "../../Engine/Scene/Scene.h"
+#include "../../Engine/Components/Base/GameObject.h"
+
+/*2025.07.28 - 안성빈
+ 게임 로직 테스트 씬
+ 나중에 사용할 Scene은 따로 만들예정
+
+*/
+
+class GameLogicTestScene : public Scene
+{
+public:
+	GameLogicTestScene() {}
+	~GameLogicTestScene() = default;
+protected:
+	void OnEnterImpl() override;
+	void OnExitImpl() override;
+	void UpdateImpl() override;
+
+private:
+	GameObject* menuObj;
+
+};
+
---
+++ BinGameTestContents/LogicTestApplication/LogicTestApplication.cpp
@@ -0,0 +1,27 @@
+#include "LogicTestApplication.h"
+#include "../../Engine/Utils/Singleton.h"
+#include "../../Engine/Scene/SceneManager.h"
+#include "../GameLogicTestScene/GameLogicTestScene.h"
+
+void LogicTestApplication::LogicTestApplication::Initialize()
+{
+	// 1. SceneManager에 Scene 추가하기
+	GameLogicTestScene = new GameLogicTestScene();	// MainMenu 씬 객체 생성
+	Singleton<SceneManager>::GetInstance().AddScene(GameLogicTestScene); // 씬 추가
+	Singleton<SceneManager>::GetInstance().Init();	// SceneManager 초기화 -> currentScene 초기화 및 RenderSystem의 RenderLayer 초기화
+}
+
+void LogicTestApplication::LogicTestApplication::Render()
+{
+	__super::Render();
+}
+
+void LogicTestApplication::LogicTestApplication::Uninitialize()
+{
+	__super::Uninitialize();
+}
+
+void LogicTestApplication::LogicTestApplication::Update()
+{
+	__super::Update();
+}
