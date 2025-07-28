b5e964f954c265f35419c6977e7bb0ded432ed91
Dareuta <jk99333@gmail.com>
2025-07-28 21:09:14 +0900

    엔진 리딩 + 로직 구상중

--- WorkSpace/HSK/HSKScene.cpp
+++ WorkSpace/HSK/HSKScene.cpp
@@ -1,17 +1,34 @@
 #include "HSKScene.h"
+#include "Components/Logic/InputSystem.h"
 
 using namespace HSK;
 void HSKScene::OnEnterImpl()
 {
+	/*
 	obj = new GameObject();
 	obj->SetName("test");
 	AddGameObject(obj);
+	*/
+
+
+
+
 }
 
 void HSKScene::OnExitImpl()
 {
+
+
+
+
+
 }
 
 void HSKScene::UpdateImpl()
 {
+
+
+
+
+
 }
