3fa17aee9428d90aa54b9dda8898e086ac9ab317
Popimuty <bin9825aaa@gmail.com>
2025-07-29 16:55:05 +0900

    데이터 읽어오기 완료
    데이터 읽기 + 저장 함수 완료
    각 엑셀 파일의 클래스 추가중

--- WorkSpace/ASB/LiveObject/Enemy.h
+++ WorkSpace/ASB/LiveObject/Enemy.h
@@ -7,9 +7,11 @@ public:
 	Enemy() {};
 	~Enemy() {};
 public:
-	float GetTotalImbalance() override ;
+	float GetTotalImbalance() override { return TotalImbalance; };
+	std::string GetDifficulty() override { return Difficulty; } // 난이도 반환
 
 private:
-	float TotalImbalance = 0.0f;
+	float TotalImbalance = 0.0f; // 적의 총 채간
+	std::string Difficulty;       // 적의 난이도 
 };
 
--- WorkSpace/ASB/game.cpp
+++ WorkSpace/ASB/game.cpp
@@ -1,5 +1,9 @@
 ﻿#include <windows.h>
 #include "LogicTestApplication/LogicTestApplication.h"
+#include <io.h>
+#include <fcntl.h>
+
+
 // ASB
 
 /// <summary>
@@ -11,6 +15,24 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_ int       nCmdShow)
 {
     CoInitialize(nullptr);  // COM 초기화
+	AllocConsole();
+
+	// 표준 출력 리디렉션
+	FILE* fp;
+    
+    errno_t err1 = freopen_s(&fp, "CONOUT$", "w", stdout); // std::cout
+    errno_t err2 = freopen_s(&fp, "CONOUT$", "w", stderr); // std::cerr
+    errno_t err3 = freopen_s(&fp, "CONIN$", "r", stdin);   // std::cin
+	if (err1 != 0) {MessageBoxW(nullptr, L"freopen_s 실패", L"Error", MB_OK);}
+	if (err2 != 0) {MessageBoxW(nullptr, L"freopen_s 실패", L"Error", MB_OK);}
+	if (err3 != 0) {MessageBoxW(nullptr, L"freopen_s 실패", L"Error", MB_OK);}
+	
+
+
+	std::cout << "콘솔 출력 테스트 - 한글" << std::endl;
+    std::cout.flush();
+	//MessageBoxW(nullptr, L"GUI 창입니다.", L"WinMain", MB_OK);
+
 
     // app.Initialize();
     // app.Run();
@@ -21,7 +43,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     TestApplication.Render();
     TestApplication.Uninitialize();
 
-
+    FreeConsole(); // 필요 없으면 종료 시 콘솔 제거
     CoUninitialize();        // COM 해제
     return (int)1;
 }
