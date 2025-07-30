8d7d7a97197ab54d4389b2da3aaab8df21423409
mob954325 <lsh954325@gmail.com>
2025-07-30 21:09:10 +0900

    feat - Random 함수 추가
    ### 추가
    1. 랜덤 범위 함수, 단위 원 랜덤 함수 추가

---
+++ Engine/Math/GameRandom.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Vector2.h"
+
+/// 25.07.30 | 작성자 : 이성호
+/// 랜덤 값 편하게 꺼낼려고 만듦
+/// 원 범위 렌덤, 범위 랜덤 등등
+
+static class GameRandom
+{
+public:
+	static float RandomRange(float min, float max);
+	static Vector2 RandomInsideUnitCircle();
+};
+
