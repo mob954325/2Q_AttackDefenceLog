63d7444a8d00c4b27252cf7fe734603b15987487
mob954325 <lsh954325@gmail.com>
2025-07-31 15:37:16 +0900

    feat - 라인랜더러 추가
    ### 추가
    경로 : Components/Rendering/LineRenderer.h
    두 지점을 이용해 선을 그리는 랜더링 컴포넌트
    
    ### 사용 방법
    1. LineRenderer를 게임 오브젝트에 추가
    2. SetPosition1, SetPosition2를 이용해 지점 설정
    
    참고 - 위치는 owner의 위치를 기준으로 선을 그림.

---
+++ Engine/Components/Rendering/LineRenderer.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "Components/Rendering/RenderComponent.h"
+#include "Math/Vector2.h"
+
+class LineRenderer : public RenderComponent
+{
+public:
+	void OnStart() override;
+	void OnDestroy() override;
+
+	void SetPosition1(Vector2 pos);
+	Vector2 GetPosition1();
+	void SetPosition2(Vector2 pos);
+	Vector2 GetPosition2();
+
+	void Render(D2DRenderManager* render) override;
+	void SetWidth(FLOAT width);
+	void SetStyle(ID2D1StrokeStyle* style);
+
+private:
+	D2D1_RECT_F rect{};
+
+	ID2D1SolidColorBrush* brush{};
+	FLOAT width = 0.8f;
+	ID2D1StrokeStyle* strokeStyle = (ID2D1StrokeStyle*)0;
+
+	Vector2 position1{};
+	Vector2 position2{};
+};
+
