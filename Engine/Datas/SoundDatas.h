c660f107bb46bd380c539c3c0c4dbaac25679058
Kimjunghyeon <130948037+racoonisdog@users.noreply.github.com>
2025-08-05 20:27:53 +0900

    csvloader추가 및 사용자가 구성해야할 struct 예시 추가
    csvloader는 csv데이터를 읽어들여서 wstring으로 저장만 해주는 함수
    필요한 데이터의 자료형 변화는 struct 내부에서 구현해줘야함
    struct 내부에 필수로 구현해야하는것들
    1. 읽어들일 데이터 자료형과 객체
    2. void SetData(const std::vector<std::wstring>& row)함수
    이 함수에 넘겨받은 row를 자신이 가진 객체에 값복사를 하는데 이때 자료형이 wstring이 아니라면 변환함수를 사용자가 작성해줘야함
    3. 한줄에 읽어들일 목록갯수를 struct에서 지정해야함 (if (row.size() < 3) return; 이구문)

--- Engine/Datas/SoundDatas.h
+++ Engine/Datas/SoundDatas.h
@@ -1,12 +1,32 @@
-#pragma once
+﻿#pragma once
 #include <string>
+#include <vector>
 #include "External/FMod/fmod.hpp"
+
+/* 25.08.05 | 작성자 : 김정현
+csv로 읽어올 데이터를 struct로 미리구현
+그리고 읽어올 데이터가 wstring이 아닐경우 SetData에 멤버변수에 맞게 들어가도록 변환함수 작성이 필요함
+if (row.size() < 3) return; 는 컬럼 수가 3개 미만이면 데이터를 무시하고 다음 줄로 넘어간다는 뜻
+즉 struct에서 읽을 컬럼 수를 정하고 읽어서 넣도록함 ( 읽을 수의 책임은 struct에 )
+FMOD_MODE ParseFmodMode(const std::wstring& str); 는 FMOD 내용을 FMOD로 치환하기 위한 함수
+std::wstring StripQuotes(const std::wstring& input); 는 \"\" 같은 불필요한 값을 없애는 함수
+*/
+
+FMOD_MODE ParseFmodMode(const std::wstring& str);
+std::wstring StripQuotes(const std::wstring& input);
 
 struct SoundResource
 {
 	std::wstring id;
 	std::wstring path;
 	FMOD_MODE mode;
+
+	void SetData(const std::vector<std::wstring>& row) {
+		if (row.size() < 3) return;
+		id = row[0];
+		path = StripQuotes(row[1]);
+		mode = ParseFmodMode(row[2]);  // 문자열을 열거형으로 파싱
+	}
 };
 
 struct SoundInfo
---
+++ Engine/Resources/Loaders/CsvLoader.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+#include "Datas/SoundDatas.h"
+#include "Utils/StringConvert.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+/*
+// 25.08.05 | 작성자 : 김정현
+csv에 , 를 기준으로 끊어서 저장
+\"\" 와 같은 불필요한 값을 읽었을때 SetData에서 제거하는 함수 구현으로 제거 ( LoadCSV는 CSV파일을 정말 읽기만해서 던져주는 역할 )
+int 매개변수(스킵할 줄수)를 던져줘서 몇번째줄 부터 읽도록 할 수 있음
+*/
+template<typename T >
+struct LoadCSV
+{
+	static void SetCSV(const std::wstring& path, std::vector<T>& outList, int skipLine = 1) {
+		std::ifstream file(path);
+		if (!file.is_open()) {
+			std::wcerr << "파일 열기 실패: " << path << std::endl;
+			return;
+		}
+
+		std::string line;
+		int countLine = 0;
+
+
+		while (std::getline(file, line)) {
+			if (countLine++ < skipLine) continue;
+
+			std::wstring wline = StringConvert::UTF8ToWString(line);
+			std::wstringstream ss(wline);
+			std::wstring cell;
+			std::vector<std::wstring> row;
+
+			while (std::getline(ss, cell, L',')) 
+			{
+				row.push_back(cell);
+			}
+
+			T tmpData;
+			tmpData.SetData(row);
+			outList.push_back(tmpData);
+		}
+
+		file.close();
+	}
+};
