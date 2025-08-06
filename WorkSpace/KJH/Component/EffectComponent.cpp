46ee7f2855d762393efc7e8b42aba1d51f48a821
Kimjunghyeon <130948037+racoonisdog@users.noreply.github.com>
2025-08-06 15:43:48 +0900

    이펙트component 작업중
    json업로드 까지 완료 임시저장으로 커밋

---
+++ WorkSpace/KJH/Component/EffectComponent.cpp
@@ -0,0 +1,22 @@
+﻿#include "EffectComponent.h"
+#include "Resources/Loaders/SpriteSheetLoader.h"
+
+void EffectComponent::OnCreate()
+{
+	SpriteSheetLoader::LoadSpriteSheet(L"../../Resource/Particles/effectsheet.png", SpriteData);
+}
+
+void EffectComponent::OnStart()
+{
+
+}
+
+void EffectComponent::OnDestroy()
+{
+
+}
+
+void EffectComponent::Update()
+{
+
+}
---
+++ WorkSpace/KJH/Component/EffectComponent.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "Components/Logic/ScriptComponent.h"
+#include "Datas/SpriteDatas.h"
+#include "Resources/BitmapResource.h"
+
+class EffectComponent : public ScriptComponent
+{
+public :
+	void OnCreate() override;
+	void OnStart() override;
+	void OnDestroy() override;
+	void Update() override;
+
+private:
+	SpriteSheet SpriteData;
+
+};
+
