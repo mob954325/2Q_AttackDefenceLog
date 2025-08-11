71295a78eca5e84b075a1232ec9b5bef45b8f20a
mob954325 <lsh954325@gmail.com>
2025-08-11 15:17:53 +0900

    feat - bitmapRenderer 마스킹 기능 추가
    ### 사용 방법
    1. SetClipingPosition(std::vector<Vector2>)를 통해 그릴 위치 값을 설정한다.
    
    설정할 때 인덱스 순서대로 라인을 그리며 도형을 만든다.

--- Engine/Components/Rendering/BitmapRenderer.cpp
+++ Engine/Components/Rendering/BitmapRenderer.cpp
@@ -17,6 +17,15 @@ void BitmapRenderer::Render(D2DRenderManager* manager)
 		
 		if (isFlip) mat.m11 = -mat.m11;
 
+		// layer
+		if (!points.empty())
+		{
+			D2D1_MATRIX_3X2_F layerMat = D2D1::Matrix3x2F::Identity();
+			manager->SetRenderTransform(layerMat);
+			renderManager->CreateLayer(&layer); // 레이어 생성
+			renderManager->PushLayer(D2D1::InfiniteRect(), pathGeometry.Get(), layer); // 레이어 추가
+		}
+
 		manager->SetRenderTransform(mat);
 
 		if (!useCustomRect)
@@ -30,8 +39,12 @@ void BitmapRenderer::Render(D2DRenderManager* manager)
 		{
 			manager->DrawBitmap(m_bitmapResource->GetBitmap(), destRect, srcRect, capacity); // 0730 : 작성자 : 이성호, rect로 직접 수정할 수 있게 코드 추가
 		}
+
+		if (!points.empty()) renderManager->PopLayer(); // 레이어 제거
+		layer->Release();
+		layer = NULL;
 	}
-	else
+	else // 예외 처리
 	{
 		if (m_bitmapResource == nullptr)
 		{
@@ -108,4 +121,43 @@ void BitmapRenderer::SetCapacity(float value)
 float BitmapRenderer::GetCapacity()
 {
 	return capacity;
+}
+
+void BitmapRenderer::SetClipingPosition(const std::vector<Vector2>& points)
+{
+	HRESULT hr = S_OK;
+
+	this->points.clear();
+
+	if (points.size() < 3)
+	{
+		std::cerr << "[BitmapRenderer] - 전달 받은 위치값이 부족합니다. 최소 3개 이상을 전달해주세요." << std::endl;
+		return;
+	}
+
+	this->points = points;
+
+	renderManager->CreatePathGeometry(&pathGeometry);
+	pathGeometry->Open(&pSink);
+
+	pSink->SetFillMode(D2D1_FILL_MODE_WINDING);
+
+	pSink->BeginFigure
+	(
+		D2D1::Point2F(points[0].x, points[0].y), 
+		D2D1_FIGURE_BEGIN_FILLED
+	);
+
+	int size = points.size();
+	for (int i = 1; i < size; i++)
+	{
+		pSink->AddLine(D2D1::Point2F(points[i].x, points[i].y));
+	}
+
+	pSink->EndFigure(D2D1_FIGURE_END_CLOSED);
+
+	hr = pSink->Close();
+	assert(SUCCEEDED(hr));
+	pSink->Release();
+	pSink = nullptr;
 }
