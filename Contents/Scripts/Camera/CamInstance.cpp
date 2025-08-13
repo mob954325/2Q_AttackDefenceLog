4f70ee2da40ad5b4a3d4ac4e8a33fe08651c46d6
Dareuta <jk99333@gmail.com>
2025-08-13 19:43:23 +0900

    feat - 정현씨 카메라 흔드는 컴포넌트 추가

---
+++ Contents/Scripts/Camera/CamInstance.cpp
@@ -0,0 +1,86 @@
+﻿#include "CamInstance.h"
+#include "Components/Base/GameObject.h"
+#include "Components/Camera/CameraManager.h"
+#include "Math/Vector2.h" 
+
+
+void CamInstance::OnUpdate()
+{
+	if (shakeType != ShakeType::None && counttime < maxtimer)
+	{
+		counttime += Singleton<GameTime>::GetInstance().GetDeltaTime();
+		switch (shakeType)
+		{
+		case ShakeType::X:
+			Came->ShakeCamera(counttime, 0, shakecalcutype, maxtimer);
+			break;
+		case ShakeType::Y:
+			Came->ShakeCamera(0, counttime, shakecalcutype, maxtimer);
+			break;
+		case ShakeType::XY:
+			Came->SetX_YValue(false);
+			Came->ShakeCamera(counttime, counttime , shakecalcutype , maxtimer);
+			break;
+		case ShakeType::X_Y:
+			Came->SetX_YValue(true);
+			Came->ShakeCamera(counttime, counttime , shakecalcutype , maxtimer);
+			break;
+		}
+	}
+	else if (shakeType != ShakeType::None && counttime >= maxtimer)
+	{
+		Came->SetShakeActive(false);
+		Came->ShakeCamera(0, 0 , shakecalcutype, maxtimer);
+		SetReset();
+	}
+}
+
+void CamInstance::OnCreate()
+{
+	Came = owner->AddComponent<Camera>();
+	Singleton<CameraManager>::GetInstance().Register(new CameraInfo(Came->GetPriority(), Came));
+}
+
+void CamInstance::OnStart()
+{
+
+}
+
+void CamInstance::OnDestroy()
+{
+
+}
+
+void CamInstance::SetWaveValue(float MaxTime, float Period, float Amplitude, float MaxX, float MaxY)
+{
+	maxtimer = MaxTime;
+	Came->SetWave(Period, Amplitude);
+	Came->SetMaxWave(MaxX, MaxY);
+}
+
+void CamInstance::CallCamShake(ShakeType type)
+{
+	shakeType = type;
+	Came->SetShakeActive(true);
+	counttime = 0;
+}
+
+void CamInstance::SetReset()
+{
+	shakeType = ShakeType::None;
+}
+
+void CamInstance::SetSlowToFast()
+{
+	shakecalcutype = ShakeCalcu::SlowToFast;
+}
+
+void CamInstance::SetFastToSlow()
+{
+	shakecalcutype = ShakeCalcu::FastToSlow;
+}
+
+void CamInstance::SetSlowFastSlow()
+{
+	shakecalcutype = ShakeCalcu::SlowFastSlow;
+}
