e645c9056215c79e25757748ab5e4d935405cf73
Popimuty <bin9825aaa@gmail.com>
2025-08-14 06:40:29 +0900

    fix - 기세게이지 감소 데이터 넣음

--- Contents/Scripts/LiveObject/Player.cpp
+++ Contents/Scripts/LiveObject/Player.cpp
@@ -89,7 +89,7 @@ void Player::OnUpdate() {
 		AddPattenLoop();		// 패턴을 추가하는 루프
 	}
 
-	// PrintConsole();		// 콘솔 호출
+	PrintConsole();		// 콘솔 호출
 	m_State->Update(); 	// 각 state별 추가로 정의된 행동 호출
 	DiffState();  		// 이전상태와 현재상태를 결정하는 함수
 	StateAct();   		// 각 state 별 행동 
