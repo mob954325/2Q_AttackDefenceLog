4d39aca122807e3e7ddea43cf9590e7ca97f70f5
Lee SeongHo <lsh954325@gmail.com>
2025-08-14 00:39:35 +0900

    feat - 스테이지 클리어 이펙트 추가

--- Contents/Scripts/SelectSeal/SelectSeal.cpp
+++ Contents/Scripts/SelectSeal/SelectSeal.cpp
@@ -2,21 +2,58 @@
 #include "Components/Base/GameObject.h"
 #include "Application/AppPaths.h"
 #include "Scene/SceneManager.h"
+#include "Utils/GameTime.h"
 
 void SelectSeal::OnCreate()
 {
 	owner->SetRenderLayer(EngineData::RenderLayer::UI);
-	owner->GetTransform().SetUnityCoords(false);
+	owner->GetTransform().SetScale(0.7f, 0.7f);
+	// owner->GetTransform().SetUnityCoords(false);
 
 	left = owner->AddComponent<BitmapRenderer>();
 	left->CreateBitmapResource(Singleton<AppPaths>::GetInstance().GetWorkingPath() + L"\\..\\Resource\\UI\\Seal\\seal_breaking_02.png");
-	left->SetOrderInLayer(1110);
-
+	left->SetUseCustomRect(true);
+	left->SetOrderInLayer(1000);
+	
 	right = owner->AddComponent<BitmapRenderer>();
 	right->CreateBitmapResource(Singleton<AppPaths>::GetInstance().GetWorkingPath() + L"\\..\\Resource\\UI\\Seal\\seal_breaking_01.png");
-	right->SetOrderInLayer(1110);
+	right->SetUseCustomRect(true);
+	right->SetOrderInLayer(1000);
 }
 
 void SelectSeal::OnStart()
 {
+}
+
+void SelectSeal::OnUpdate()
+{
+	if (!isRighDone)
+	{
+		if (timer < maxTimer)
+		{
+			float rectY = left->GetResource()->GetBitmap()->GetSize().height * timer / maxTimer;
+
+			right->SetDestRect({ 0, 0, right->GetResource()->GetBitmap()->GetSize().width, rectY });
+			right->SetSrcRect({ 0, 0, right->GetResource()->GetBitmap()->GetSize().width, rectY });
+
+			timer += Singleton<GameTime>::GetInstance().GetDeltaTime();
+		}
+		else
+		{
+			isRighDone = true;
+			timer = 0.0f;
+		}
+	}
+	else // left
+	{
+		if (timer < maxTimer)
+		{
+			float rectY = left->GetResource()->GetBitmap()->GetSize().height * timer / maxTimer;
+
+			left->SetDestRect({ 0, 0, left->GetResource()->GetBitmap()->GetSize().width, rectY });
+			left->SetSrcRect({ 0, 0, left->GetResource()->GetBitmap()->GetSize().width, rectY });
+
+			timer += Singleton<GameTime>::GetInstance().GetDeltaTime();
+		}
+	}
 }
