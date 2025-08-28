a6d1709066d86404156aea427fb9fe960db25a83
Kimjunghyeon <130948037+racoonisdog@users.noreply.github.com>
2025-08-28 19:46:25 +0900

    fix - 가드이펙트 오류 수정본
    수정

--- Contents/Scripts/Effect/EnemyAttackEffect.cpp
+++ Contents/Scripts/Effect/EnemyAttackEffect.cpp
@@ -27,6 +27,23 @@ void EnemyAttackEffect::OnUpdate()
 			p->Reset();
 		}
 	}
+
+	for (size_t i = 0; i < AnimeListE.size(); ++i) {
+		auto& state = AnimeStatesE[i];
+		if (!state.playing) continue;
+
+		state.t += dt;
+		if (state.t >= state.duration) {
+			state.playing = false;
+			state.t = 0.0f;
+
+			auto* r = AnimeListE[i]->GetComponent<AnimationRenderer>();
+			r->SetActive(false);
+			auto* p = r->GetAnimationPlayer();
+			p->Pause();
+			p->Reset();
+		}
+	}
 }
 
 void EnemyAttackEffect::OnCreate()
@@ -51,6 +68,27 @@ void EnemyAttackEffect::OnCreate()
 	AnimeStates.resize(AnimeList.size());
 
 	Animeduration = AnimeList[0]->GetComponent<AnimationRenderer>()->GetAnimationPlayer()->GetAnimationClip().duration;
+
+	for (size_t i = 0; i < 10; i++)
+	{
+		GameObject* obj = new GameObject();
+		obj->SetRenderLayer(EngineData::RenderLayer::UI);
+		obj->AddComponent<AnimationRenderer>();
+		obj->SetName(std::string("EnemyAttackEffect") + std::to_string(i));
+		Singleton<SceneManager>::GetInstance().GetCurrentScene()->AddGameObject(obj);
+		obj->GetComponent<AnimationRenderer>()->SetActive(false);
+		obj->GetComponent<AnimationRenderer>()->CreateBitmapResource(Singleton<AppPaths>::GetInstance().GetWorkingPath() + L"\\..\\Resource\\Sprites\\Effect\\PlayerDefence\\defence_player_enemy_spreadsheet.png");
+		obj->GetComponent<AnimationRenderer>()->SetSpriteSheet(Singleton<AppPaths>::GetInstance().GetWorkingPath() + L"\\..\\Resource\\Json\\defence\\defence_player_enemy_spreadsheet_sprites.json");
+		obj->GetComponent<AnimationRenderer>()->SetAnimationClip(Singleton<AppPaths>::GetInstance().GetWorkingPath() + L"\\..\\Resource\\Json\\defence\\defence_player_enemy_spreadsheet_anim.json");
+		obj->GetComponent<AnimationRenderer>()->GetAnimationPlayer()->Pause();
+		obj->GetComponent<AnimationRenderer>()->SetOrderInLayer(-9);
+		obj->GetTransform().SetUnityCoords(false);
+		AnimeListE.push_back(obj);
+	}
+
+	AnimeStatesE.resize(AnimeListE.size());
+
+	AnimedurationE = AnimeListE[0]->GetComponent<AnimationRenderer>()->GetAnimationPlayer()->GetAnimationClip().duration;
 
 }
 
@@ -102,8 +140,49 @@ void EnemyAttackEffect::StopAnime(int num)
 	p->Reset();
 }
 
+//Enemy
+void EnemyAttackEffect::SetAnimePositionE(int num, const Vector2& vector)
+{
+	AnimeListE[num]->GetTransform().SetPosition(vector.x, vector.y);
+}
+
+void EnemyAttackEffect::CallAnimeE(int num, Vector2 vector)
+{
+	size_t Animesize = AnimeListE.size();
+	int a = 0;
+
+	if (num >= AnimeListE.size()) return;
+	auto* r = AnimeListE[num]->GetComponent<AnimationRenderer>();
+	r->SetActive(true);
+	auto* p = r->GetAnimationPlayer();
+	p->Play();
+	SetAnimePosition(num, vector);
+
+	auto& s = AnimeStatesE[num];
+	s.playing = true;
+	s.t = 0.0f;
+	s.duration = AnimedurationE; // 슬롯마다 다르게 가능
+}
+
+void EnemyAttackEffect::StopAnimeE(int num)
+{
+	if (num >= AnimeListE.size()) return;
+	auto& s = AnimeStatesE[num];
+	if (!s.playing) return;
+
+	s.playing = false;
+	s.t = 0.0f;
+
+	auto* r = AnimeListE[num]->GetComponent<AnimationRenderer>();
+	r->SetActive(false);
+	auto* p = r->GetAnimationPlayer();
+	p->Pause();
+	p->Reset();
+}
+
 
 void EnemyAttackEffect::StopAllAnime()
 {
 	for (size_t i = 0; i < AnimeList.size(); ++i) StopAnime(i);
+	for (size_t i = 0; i < AnimeListE.size(); ++i) StopAnime(i);
 }
