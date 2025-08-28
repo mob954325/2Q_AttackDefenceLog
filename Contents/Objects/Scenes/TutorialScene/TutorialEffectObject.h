58b9df1812aaa47b453eabfbbe16ff159f562a79
Dareuta <jk99333@gmail.com>
2025-08-28 19:26:52 +0900

    feat - TutorialEffect 추가

---
+++ Contents/Objects/Scenes/TutorialScene/TutorialEffectObject.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Components/Base/MonoBehavior.h"
+#include "Components/Rendering/BitmapRenderer.h"
+
+
+class TutorialEffectObject : public MonoBehavior
+{
+public:
+	void OnCreate() override;
+	void OnStart() override;
+	void OnUpdate() override;
+
+protected:
+	
+};
+
+
