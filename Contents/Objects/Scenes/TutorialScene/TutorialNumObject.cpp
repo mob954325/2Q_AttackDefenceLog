0116a9f703409e869eb222695c5785609b3caa48
Dareuta <jk99333@gmail.com>
2025-08-31 02:49:22 +0900

    feat - 튜토리얼 숫자 출력하는 오브젝트 제작 중

---
+++ Contents/Objects/Scenes/TutorialScene/TutorialNumObject.cpp
@@ -0,0 +1,31 @@
+﻿#include "TutorialNumObject.h"
+#include "Application/AppPaths.h"
+#include "../Engine/Scene/SceneManager.h"
+
+
+void TutorialNumObject::OnCreate()
+{
+
+}
+
+
+void TutorialNumObject::OnStart()
+{
+	
+		
+}
+
+void TutorialNumObject::OnUpdate() 
+{
+
+}
+
+void TutorialNumObject::Show()
+{
+
+}
+
+void TutorialNumObject::Hide()
+{
+	
+}
