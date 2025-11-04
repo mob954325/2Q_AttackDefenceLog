71c01c76d5fe2f6248ae1d6099ca5c6699fe749c
Dareuta <jk99333@gmail.com>
2025-11-04 19:39:27 +0900

    feat - 말풍선 전부 교체함
    기존 말풍선 배경은 투명 리소스로 교체함

--- Contents/Scripts/BubbleBox/BubbleBox2.cpp
+++ Contents/Scripts/BubbleBox/BubbleBox2.cpp
@@ -7,6 +7,8 @@
 #include "../Engine/Systems/AudioSystem.h"
 #include "Objects/Sound/SoundPlayScene.h"
 
+// 스테이지 2
+
 void BubbleBox2::OnUpdate()
 {
 	if (StartCheck) CheckInput();
@@ -27,27 +29,27 @@ void BubbleBox2::OnCreate()
 	Text_02 = owner->AddComponent<BitmapRenderer>();
 	Text_03 = owner->AddComponent<BitmapRenderer>();
 	Text_04 = owner->AddComponent<BitmapRenderer>();
-	Text_05 = owner->AddComponent<BitmapRenderer>();
+	//Text_05 = owner->AddComponent<BitmapRenderer>();
 
 	TextBox->CreateBitmapResource(Singleton<AppPaths>::GetInstance().GetWorkingPath() + L"\\..\\Resource\\Sprites\\UI\\bubbleBox\\textbox.png");
 	Text_01->CreateBitmapResource(Singleton<AppPaths>::GetInstance().GetWorkingPath() + L"\\..\\Resource\\Sprites\\UI\\BoxText\\2stage\\01.png");
 	Text_02->CreateBitmapResource(Singleton<AppPaths>::GetInstance().GetWorkingPath() + L"\\..\\Resource\\Sprites\\UI\\BoxText\\2stage\\02.png");
 	Text_03->CreateBitmapResource(Singleton<AppPaths>::GetInstance().GetWorkingPath() + L"\\..\\Resource\\Sprites\\UI\\BoxText\\2stage\\03.png");
-	Text_04->CreateBitmapResource(Singleton<AppPaths>::GetInstance().GetWorkingPath() + L"\\..\\Resource\\Sprites\\UI\\BoxText\\2stage\\05.png");
-	Text_05->CreateBitmapResource(Singleton<AppPaths>::GetInstance().GetWorkingPath() + L"\\..\\Resource\\Sprites\\UI\\BoxText\\2stage\\05.png");
+	Text_04->CreateBitmapResource(Singleton<AppPaths>::GetInstance().GetWorkingPath() + L"\\..\\Resource\\Sprites\\UI\\BoxText\\2stage\\04.png"); // 이거 왜 05 쓰고있었음...?(충격)
+	//Text_05->CreateBitmapResource(Singleton<AppPaths>::GetInstance().GetWorkingPath() + L"\\..\\Resource\\Sprites\\UI\\BoxText\\2stage\\05.png");
 
 	TextBox->SetActive(true);
 	Text_01->SetActive(true);
 	Text_02->SetActive(false);
 	Text_03->SetActive(false);
 	Text_04->SetActive(false);
-	Text_05->SetActive(false);
+	//Text_05->SetActive(false);
 
 	Text_01->SetOrderInLayer(19000);
 	Text_02->SetOrderInLayer(19000);
 	Text_03->SetOrderInLayer(19000);
 	Text_04->SetOrderInLayer(19000);
-	Text_05->SetOrderInLayer(19000);
+	//Text_05->SetOrderInLayer(19000);
 
 	count = 0;
 	StartCheck = true;
@@ -77,37 +79,28 @@ void BubbleBox2::CheckInput()
 			Text_02->SetActive(true);
 			Text_03->SetActive(false);
 			Text_04->SetActive(false);
-			Text_05->SetActive(false);
+			//Text_05->SetActive(false);
 			break;
 		case 1:
 			Text_01->SetActive(false);
 			Text_02->SetActive(false);
 			Text_03->SetActive(true);
 			Text_04->SetActive(false);
-			Text_05->SetActive(false);
+			//Text_05->SetActive(false);
 			break;
 		case 2:
 			Text_01->SetActive(false);
 			Text_02->SetActive(false);
 			Text_03->SetActive(false);
 			Text_04->SetActive(true);
-			Text_05->SetActive(false);
+			//Text_05->SetActive(false);
 			break;
 		case 3:
 			Text_01->SetActive(false);
 			Text_02->SetActive(false);
 			Text_03->SetActive(false);
 			Text_04->SetActive(false);
-			Text_05->SetActive(true);
-			break;
-		case 4:
-			TextBox->SetActive(false);
-			Text_01->SetActive(false);
-			Text_02->SetActive(false);
-			Text_03->SetActive(false);
-			Text_04->SetActive(false);
-			Text_05->SetActive(false);
-
+			//Text_05->SetActive(true);
 			StartCheck = false;
 			Singleton<GameManager>::GetInstance().SetGameState(Play);
 			/*Singleton<AudioSystem>::GetInstance().AgainstSound();*/
@@ -120,6 +113,26 @@ void BubbleBox2::CheckInput()
 			escPanel->SetInputEnable(true);
 
 			break;
+		//case 4:
+		//	TextBox->SetActive(false);
+		//	Text_01->SetActive(false);
+		//	Text_02->SetActive(false);
+		//	Text_03->SetActive(false);
+		//	Text_04->SetActive(false);
+		//	//Text_05->SetActive(false);
+
+		//	//StartCheck = false;
+		//	//Singleton<GameManager>::GetInstance().SetGameState(Play);
+		//	///*Singleton<AudioSystem>::GetInstance().AgainstSound();*/
+		//	//auto SoundCom = owner->GetQuery()->FindByName("SOUNDSTAGE");
+		//	//if (SoundCom) {
+		//	//	SoundCom->GetComponent<SoundPlayScene>()->SetKeyHandle(L"Stage02");
+		//	//	SoundCom->GetComponent<SoundPlayScene>()->PlaySound();;
+		//	//}
+
+		//	//escPanel->SetInputEnable(true);
+
+		//	break;
 		}
 		++count;
 	}
